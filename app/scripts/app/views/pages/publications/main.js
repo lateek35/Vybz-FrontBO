77e8d07b795a43b30d0def8ef20c2a702f5e3f0c
Allan RAQUIN <lateek35@gmail.com>
2016-06-05 20:14:12 +0200

    publication end

--- app/scripts/app/views/pages/publications/main.js
+++ app/scripts/app/views/pages/publications/main.js
@@ -3,24 +3,52 @@ Handlebars = require('handlebars'),
 Backbone   = require('backbone'),
 template   = require('./template.hbs');
 
+var ALaUneView = require('./aLaUne/main'),
+	CoinDesSupportersView = require('./coinDesSupporters/main');
+
+
 module.exports = Backbone.View.extend({
 
 	el: '#content',
+	tabs: '#publications .tab',
+	aLaUneBool : true,
+	aLaUne : null,
+	coinDesSupporters : null,
 
 	initialize: function(){
-		this.bindEvent();
+	},
+
+	loadTab: function(){
+		if (this.aLaUneBool) {
+			this.coinDesSupporters && this.coinDesSupporters.remove();
+			this.aLaUne = new ALaUneView();
+		}else{
+			this.aLaUne && this.aLaUne.remove();
+			this.coinDesSupporters = new CoinDesSupportersView();
+		}
 	},
 
 	bindEvent: function(){
+		$(this.tabs).on('click', this.toggleMenu.bind(this) );
 
 	},
 
 	unbindEvents: function(){
+		$(this.tabs).off('click', this.toggleMenu.bind(this) );
+	},
 
+	toggleMenu: function(e){
+		var target = e.currentTarget;
+		this.$el.find('.active').toggleClass('active', false);
+		$(target).toggleClass('active', true);
+		this.aLaUneBool = !this.aLaUneBool;
+		this.loadTab();
 	},
 
 	render: function(){
 		this.$el.html( template({tpl : 'PUBLICATIONS'}) );
+		this.bindEvent();
+		this.loadTab();
 	},
 
 	remove: function(){
